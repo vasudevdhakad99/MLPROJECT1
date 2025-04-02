2ba62c6df7a102f2b372381f45ec97ddbfe92b80
vasu <vasudevdhakad99@gmail.com>
2025-04-02 16:54:43 +0530

    commit

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+## logger is used to track the events that occur while running the program
+## so you can use logging to record the warning ,erros and other event that occur while running the program
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
+
+
+
+
