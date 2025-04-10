925fa57a07ff92b00ff99662a4815b6f6259ca3f
vasu <vasudevdhakad99@gmail.com>
2025-04-10 10:47:24 +0530

    flask update

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,9 +1,10 @@
 import sys
 import pandas as pd
+import os
 from src.exception import CustomException
 from src.utils import load_object
 
-
+#E:\MLPROJECT1\artifacts\proprocessor.pkl
 class PredictPipeline:
     def __init__(self):
         pass
@@ -11,7 +12,7 @@ class PredictPipeline:
     def predict(self,features):
         try:
             model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            preprocessor_path=os.path.join("artifacts",'preprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
